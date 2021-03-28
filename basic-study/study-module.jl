e35b54d15ef34e98758ece81081d60b61c580623
Mizuki ABE@Laptop <mizuki.abe.r4@dc.tohoku.ac.jp>
2021-03-28 11:39:05 +0000

    モジュールについて簡単なプログラムを作った

--- basic-study/study-module.jl
+++ basic-study/study-module.jl
@@ -11,6 +11,13 @@ end
 # 相対パスでモジュールGreetingsを読み込む
 using .Greetings
 
+include("modulefile.jl")
+using .TestModule
+
 hello("Julia")
 
 Greetings.goodbye("Julia")
+
+TestModule.testHoge(1, 1.0)
+
+println("TestModule.a = $(TestModule.a)")
