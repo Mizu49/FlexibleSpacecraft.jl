2c23fcd0124099b26b0ac9d973dbd6bdaa70f463
Mizu49@LabPC <mizuki.abe.r4@dc.tohoku.ac.jp>
2021-04-23 07:17:55 +0000

    `launch.json`を作成

--- 1_attitude-control/attitude-dynamics.jl
+++ 1_attitude-control/attitude-dynamics.jl
@@ -25,16 +25,10 @@ I = diagm(0 => [1.0, 1.0, 2.0])
 M = [0 0 0]'
 
 
-
-
 omegaBA = zeros(3, simDataNum)
 omegaBA[:,1] = [1.0 0.0 1.0]';
 
-cross(omegaBA[:,1], I)
 
 for loopCounter = 1:simDataNum-1
-    k1 = inv(I)*(M - dot(kron(omegaBA[:,loopCounter], I), omegaBA[:,loopCounter]) )
-
-
-    println(omegaBA[1, counter])    
+    println(loopCounter)    
 end
