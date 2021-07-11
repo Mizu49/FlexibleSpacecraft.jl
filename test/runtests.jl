848e12314061c010a3f5c67e65717270e1e8a324
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2021-07-11 12:56:30 +0000

    add test script in `runtest.jl` #19
    - add test simulation script
    - add quaternion testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,91 @@ include("../src/FlexibleSpacecraft.jl")
 using .FlexibleSpacecraft
 using Test
 
-@test 1 + 1 == 2
-# easy test
+"""
+    function testQuaternionConstraint(quaternion)
+
+Check if quaternion vector satisfies constraint
+"""
+function testQuaternionConstraint(quaternion)
+
+    datanum = size(quaternion, 2)
+
+    # initialize BitArray for evaluation
+    logicalArray = falses(datanum)
+
+    # evaluate constraint of quaiteratively
+    for cnt = 1:datanum
+        constraint = quaternion[1, cnt]^2 + quaternion[2, cnt]^2 + quaternion[3, cnt]^2 + quaternion[4, cnt]^2
+
+        if constraint ≈ 1 rtol = 1e-9
+            logicalArray[cnt] = true
+        else
+            logicalArray[cnt] = false
+        end
+    end
+
+    return all(logicalArray)
+end
+
+# Test simulation script
+@testset "Z-axis rotation" begin
+
+    # inertia matrix
+    inertia = diagm([1.0, 1.0, 2.0])
+
+    # Disturbance disturbance
+    disturbance = [0.0, 0.0, 0.0]
+
+    # Dynamics model (mutable struct)
+    model = RigidBodyAttitudeDynamics.DynamicsModel(inertia, disturbance)
+
+    # Sampling period of simulation (second)
+    Ts = 1e-2
+
+    # Time length of simulation (second)
+    simulation_time = 60
+
+    # 時刻
+    time = 0:Ts:simulation_time
+
+    # Numbers of simulation data
+    simu_data_num = round(Int, simulation_time/Ts) + 1;
+
+    # Coordinate system of a
+    coordinateA = TimeLine.Coordinate(
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    )
+
+    # Coordinate system of b
+    coordinateB = TimeLine.init_coordinate_array(simu_data_num, coordinateA)
+
+    omegaBA = TimeLine.init_angular_velocity_array(simu_data_num, [0, 0, 1])
+
+    quaternion = TimeLine.init_quaternion_array(simu_data_num, [0, 0, 0, 1])
+
+    for loopCounter = 0:simu_data_num - 2
+
+        # println(loopCounter + 1)
+
+        # Extract body fixed frame at current time step
+        currentCoordB = hcat(coordinateB.x[:,loopCounter + 1] , coordinateB.y[:,loopCounter + 1], coordinateB.z[:,loopCounter + 1])
+
+        omegaBA[:, loopCounter + 2] = RigidBodyAttitudeDynamics.calc_angular_velocity(model, time[loopCounter + 1], omegaBA[:, loopCounter + 1], Ts, currentCoordB)
+
+        quaternion[:, loopCounter + 2] = RigidBodyAttitudeDynamics.calc_quaternion(omegaBA[:,loopCounter + 1], quaternion[:, loopCounter + 1], Ts)
+
+        C = RigidBodyAttitudeDynamics.calc_transformation_matrix(quaternion[:, loopCounter + 1])
+
+        coordinateB.x[:, loopCounter + 2] = C * coordinateA.x
+        coordinateB.y[:, loopCounter + 2] = C * coordinateA.y
+        coordinateB.z[:, loopCounter + 2] = C * coordinateA.z
+
+    end
+
+    @test testQuaternionConstraint(quaternion)
+
+end
+
+
