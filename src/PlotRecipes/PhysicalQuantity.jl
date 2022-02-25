dd1d816e7cdfe7bbb677cd9ac08e7a4a584b3a60
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-02-25 05:52:07 +0000

    modify plot recipe #80

--- src/PlotRecipes/PhysicalQuantity.jl
+++ src/PlotRecipes/PhysicalQuantity.jl
@@ -18,9 +18,9 @@ function angularvelocities(time::StepRangeLen, angularvelocity::Vector{StaticArr
     plotlyjs()
 
     plt = plot();
-    plt = plot!(plt, time, angularvelocity, 1, timerange = timerange, ylabelname = "Angular velocity (rad/s)");
-    plt = plot!(plt, time, angularvelocity, 2, timerange = timerange, ylabelname = "Angular velocity (rad/s)");
-    plt = plot!(plt, time, angularvelocity, 3, timerange = timerange, ylabelname = "Angular velocity (rad/s)");
+    plt = plot!(plt, time, angularvelocity, 1, timerange = timerange, ylabelname = "Angular velocity (rad/s)", datalabel = "x-axis");
+    plt = plot!(plt, time, angularvelocity, 2, timerange = timerange, ylabelname = "Angular velocity (rad/s)", datalabel = "y-axis");
+    plt = plot!(plt, time, angularvelocity, 3, timerange = timerange, ylabelname = "Angular velocity (rad/s)", datalabel = "z-axis");
 
     return plt
 end
@@ -36,9 +36,9 @@ function eulerangles(time::StepRangeLen, eulerangle::Vector{StaticArrays.SVector
     plotlyjs()
 
     plt = plot();
-    plt = plot!(plt, time, eulerangle, 1, timerange = timerange, ylabelname = "Euler angle (rad)");
-    plt = plot!(plt, time, eulerangle, 2, timerange = timerange, ylabelname = "Euler angle (rad)");
-    plt = plot!(plt, time, eulerangle, 3, timerange = timerange, ylabelname = "Euler angle (rad)");
+    plt = plot!(plt, time, eulerangle, 1, timerange = timerange, ylabelname = "Euler angle (rad)", datalabel = "roll");
+    plt = plot!(plt, time, eulerangle, 2, timerange = timerange, ylabelname = "Euler angle (rad)", datalabel = "pitch");
+    plt = plot!(plt, time, eulerangle, 3, timerange = timerange, ylabelname = "Euler angle (rad)", datalabel = "yaw");
 
     return plt
 end
@@ -53,33 +53,22 @@ function quaternions(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector
     plotlyjs()
 
     plt = plot();
-    plt = plot!(plt, time, quaternion, 1, timerange = timerange, ylabelname = "Quaternion");
-    plt = plot!(plt, time, quaternion, 2, timerange = timerange, ylabelname = "Quaternion");
-    plt = plot!(plt, time, quaternion, 3, timerange = timerange, ylabelname = "Quaternion");
-    plt = plot!(plt, time, quaternion, 4, timerange = timerange, ylabelname = "Quaternion");
+    plt = plot!(plt, time, quaternion, 1, timerange = timerange, ylabelname = "Quaternion", datalabel = "q1");
+    plt = plot!(plt, time, quaternion, 2, timerange = timerange, ylabelname = "Quaternion", datalabel = "q2");
+    plt = plot!(plt, time, quaternion, 3, timerange = timerange, ylabelname = "Quaternion", datalabel = "q3");
+    plt = plot!(plt, time, quaternion, 4, timerange = timerange, ylabelname = "Quaternion", datalabel = "q4");
 
     return plt
 end
 
-@recipe function f(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector{4, Float64}}, index::Integer; timerange = (0, 0), ylabelname = "No name")
+@recipe function f(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector{4, Float64}}, index::Integer; timerange = (0, 0), ylabelname = "No name", datalabel::String = "")
     if !(1 <= index <= 4)
         throw(BoundsError(quaternion[1], index))
     end
 
     xguide --> "Time (s)"
     yguide --> ylabelname
-
-    if index == 1
-        label --> "q1"
-    elseif index == 2
-        label --> "q2"
-    elseif index == 3
-        label --> "q3"
-    elseif index == 4
-        label --> "q4"
-    else
-        throw(ArgumentError("argument `index` is set improperly"))
-    end
+    label --> datalabel
 
     # get the index for data
     dataindex = TimeLine.getdataindex(timerange, convert(Float64, time.step))
@@ -87,20 +76,11 @@ end
     return time[dataindex], quaternion[dataindex, index]
 end
 
-@recipe function f(time::StepRangeLen, angularvelocity::Vector{StaticArrays.SVector{3, Float64}}, axisindex::Integer; timerange = (0, 0), ylabelname = "No name")
+@recipe function f(time::StepRangeLen, angularvelocity::Vector{StaticArrays.SVector{3, Float64}}, axisindex::Integer; timerange = (0, 0), ylabelname = "No name", datalabel::String = "")
 
     xguide --> "Time (s)"
     yguide --> ylabelname
-
-    if axisindex == 1
-        label --> "x-axix"
-    elseif axisindex == 2
-        label --> "y-axis"
-    elseif axisindex == 3
-        label --> "z-axis"
-    else
-        throw(ArgumentError("argument `axisindex` is set improperly"))
-    end
+    label --> datalabel
 
     # get the index for data
     dataindex = TimeLine.getdataindex(timerange, convert(Float64, time.step))
