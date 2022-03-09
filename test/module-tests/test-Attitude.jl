bf29917a2eacc34a1c4f94ffb753a99195b71114
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-03-09 12:12:11 +0000

    modify `@test` condition for `test-Attitude.jl` #78

--- test/module-tests/test-Attitude.jl
+++ test/module-tests/test-Attitude.jl
@@ -9,11 +9,11 @@ euler_nominal = [0, 0, 0]
 
 @testset "Attitude representation conversions" begin
 
-    @test quaternion2dcm(quaternion_nominal) == dcm_nominal
-    @test dcm2euler(dcm_nominal) == euler_nominal
-    @test euler2dcm(euler_nominal) == dcm_nominal
-    @test dcm2quaternion(dcm_nominal) == quaternion_nominal
-    @test quaternion2euler(quaternion_nominal) == euler_nominal
-    @test euler2quaternion(euler_nominal) == quaternion_nominal
+    @test isapprox(quaternion2dcm(quaternion_nominal), dcm_nominal, atol = 1e-7)
+    @test isapprox(dcm2euler(dcm_nominal), euler_nominal, atol = 1e-7)
+    @test isapprox(euler2dcm(euler_nominal), dcm_nominal, atol = 1e-7)
+    @test isapprox(dcm2quaternion(dcm_nominal), quaternion_nominal, atol = 1e-7)
+    @test isapprox(quaternion2euler(quaternion_nominal), euler_nominal, atol = 1e-7)
+    @test isapprox(euler2quaternion(euler_nominal), quaternion_nominal, atol = 1e-7)
 
 end
