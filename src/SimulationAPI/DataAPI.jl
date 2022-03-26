60a4e27ddcc8ec69137e3ebcc16c728866243677
Mizuki <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-03-26 11:48:18 +0000

    add module `DataAPI.jl` in `src` #86

---
+++ src/SimulationAPI/DataAPI.jl
@@ -0,0 +1,13 @@
+"""
+    DataAPI
+
+API submodule for handling the `DataFrame` of the simulation results
+"""
+module DataAPI
+
+using DataFrames, CSV, StaticArrays, StructArrays
+using .Frames, .TimeLine
+
+
+
+end
