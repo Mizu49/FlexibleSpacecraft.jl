ca9808383ba5a0d55008f2b74249294c8bb3302c
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-03-28 14:06:08 +0000

    comment out visualizations in `main.jl` #86

--- test/main.jl
+++ test/main.jl
@@ -25,20 +25,20 @@ println("Completed!")
 
 @test Evaluation.quaternion_constraint(attitudedata.quaternion)
 
-# fig1 = PlotRecipe.angularvelocities(time, attitudedata.angularvelocity)
-# # fig1 = PlotRecipe.angularvelocities(time, attitudedata.angularvelocity, timerange = (0, 10))
-# display(fig1)
+fig1 = PlotRecipe.angularvelocities(time, attitudedata.angularvelocity)
+# fig1 = PlotRecipe.angularvelocities(time, attitudedata.angularvelocity, timerange = (0, 10))
+display(fig1)
 
-# fig2 = PlotRecipe.quaternions(time, attitudedata.quaternion)
-# display(fig2)
+fig2 = PlotRecipe.quaternions(time, attitudedata.quaternion)
+display(fig2)
 
-# # Plot of the body frame with respect to ECI frame
-# fig3 = PlotRecipe.framegif(time, LVLHref, attitudedata.rollpitchyawframe, Tgif = 20, FPS = 8)
-# display(fig3)
+# Plot of the body frame with respect to ECI frame
+fig3 = PlotRecipe.framegif(time, LVLHref, attitudedata.rollpitchyawframe, Tgif = 20, FPS = 8)
+display(fig3)
 
-# # Plot of the euler angle
-# fig4 = PlotRecipe.eulerangles(time, attitudedata.eulerangle)
-# display(fig4)
+# Plot of the euler angle
+fig4 = PlotRecipe.eulerangles(time, attitudedata.eulerangle)
+display(fig4)
 
 outputdata = SimData(time, attitudedata, orbitdata)
 write("output", outputdata)
