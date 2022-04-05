620f6dcb9a612bef953aa1f6138130f0e0766da0
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-04-05 10:12:01 +0000

    modify test parameters in `test-SpringMass.jl` #94

--- test/module-tests/test-SpringMass.jl
+++ test/module-tests/test-SpringMass.jl
@@ -6,14 +6,21 @@ using .FlexibleSpacecraft
 paramfile = "../solararray.yml"
 
 𝐌 = [
-    10 0
-    0 20
+    100 0
+    0 50
 ]
 𝐊 = [
-    10 0
-    0 10000
+    6e4 -1e4
+    -1e4 1e4
 ]
-𝐂 = zeros(2, 2)
+C_Ma = 0
+C_Mb = 0
+C_Ka = 250
+C_Kb = 50
+𝐂 = [
+    C_Ma+C_Kb+C_Kb -C_Kb
+    -C_Kb C_Mb+C_Kb
+]
 
 𝐄ctrl = []
 𝐄dist = []
@@ -22,3 +29,5 @@ paramfile = "../solararray.yml"
     0 1 0
 ]
 
+modalmat = SpringMass._mode_decomposition(𝐌, 𝐂, 𝐊)
+println(modalmat)
