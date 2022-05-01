395ca3aa72e7ab85a5b84d62903007e898f2f9ea
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-05-01 14:07:09 +0000

    update test script for `SpringMass.jl` #94

--- test/module-tests/test-SpringMass.jl
+++ test/module-tests/test-SpringMass.jl
@@ -40,17 +40,27 @@ systemmodel = SpringMassModel(physicalsystem, Ecoupling, Ectrl, Edist)
 model = StateSpace(systemmodel)
 
 # Test the simulation feature
-Ts = 1e-3
-times = 0:Ts:50
+Ts = 1e-4
+times = 0:Ts:10
 
 datanum = size(times, 1)
 
-response = [zeros(SVector{4}) for _ in 1:datanum]
-response[1] = SVector{4}([10e-3, 10e-3, 0, 0])
+physicalstate = [zeros(SVector{4}) for _ in 1:datanum]
+physicalstate[1] = SVector{4}([1e-3, 0, 0, 0])
+state = [zeros(SVector{4}) for _ in 1:datanum]
+state[1] = physicalstate2modalstate(model, physicalstate[1])
 
-@time for cnt = 1:datanum-1
-    response[cnt+1] = updatestate(model, Ts, times[cnt], response[cnt], zeros(3), 0, 0)
+@time for cnt = 1:datanum
+    # Calculate the state in physical coordinate
+    physicalstate[cnt] = modalstate2physicalstate(model, state[cnt])
+
+    if cnt != datanum
+        # Update the state and proceed to the next computation
+        state[cnt+1] = updatestate(model, Ts, times[cnt], state[cnt], zeros(3), 0, 0)
+    end
 end
 
-@time plot(times, getindex.(response, 1))
-
+plotlyjs()
+fig = plot(times, physicalstate[:, 1])
+fig = plot!(times, physicalstate[:, 2])
+display(fig)
