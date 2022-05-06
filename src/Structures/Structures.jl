779b628acfe9fe72b839c0c8b050c0652f17adb8
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-05-06 13:46:03 +0000

    add the framework for `Structures.jl` #105

--- src/Structures/Structures.jl
+++ src/Structures/Structures.jl
@@ -5,9 +5,58 @@ module for wrapping all the submodules for the structual dynamics for flexible s
 """
 module Structures
 
-using Reexport
+using Reexport, StaticArrays
 
 include("SpringMass.jl")
 @reexport using .SpringMass
+
+export FlexibleAppendage, AllAppendages, update_structure
+
+"""
+    FlexibleAppendage
+
+struct that accomodates the configuration for a flexible appendage of the spacecraft
+"""
+struct FlexibleAppendage
+    # name of the flexible appendage
+    name::String
+    # dynamics model that is employed for simulation of this appendage
+    model::DataType
+
+    FlexibleAppendage() = begin
+
+        new(name, model)
+    end
+end
+
+"""
+    AllAppendages
+
+struct that accomodates the configurations for the multiple flexible appendages
+"""
+struct AllAppendages
+
+    appendages::SVector
+
+    AllAppendages(configs::AbstractVector) = begin
+        # numbers of appendages to be simulated
+        num_appendages = size(configs, 1)
+
+        appendages = SVector{num_appendages, Any}(undef for _ in 1:num_appendages)
+
+        new(appendages)
+    end
+end
+
+"""
+    update_structure
+
+function to calculate the time evolution of the structural motion of the flexible appendages
+"""
+function update_structure(config, angularvelocity::SVector{3, <:Real}, controlinput::AbstractVector)
+
+
+
+end
 
 end
