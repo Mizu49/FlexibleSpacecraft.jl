c7e73c8e9cd3fdf5e5c0faadd4b1a80e61ea0abe
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-05-08 13:23:10 +0000

    add function `setstructure` #102

--- src/Structures/Structures.jl
+++ src/Structures/Structures.jl
@@ -5,9 +5,33 @@ module for wrapping all the submodules for the structual dynamics for flexible s
 """
 module Structures
 
-using Reexport
+using Reexport, YAML
 
 include("SpringMass.jl")
 @reexport using .SpringMass
+
+export setstructure
+
+"""
+    setstructure
+
+API function to define the model to
+"""
+function setstructure(configfilepath::String)
+
+    lawread = YAML.load_file(configfilepath)
+
+    if haskey(lawread, "modeling") == false
+        throw(ErrorException("`model` is undefined in configuration file `$configfilepath`"))
+    end
+
+    if lawread["modeling"] == "spring-mass"
+        (structureparams, structuresimmodel) = defmodel(lawread)
+    else
+        throw(ErrorException("no matching modeling method for \"$(lawread["modeling"])\""))
+    end
+
+    return (structureparams, structuresimmodel)
+end
 
 end
