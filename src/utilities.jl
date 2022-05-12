53b0c9657b73de497f6ad7662ec270fd4934cd02
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-05-12 14:13:00 +0000

    update docstring for `utilities.jl` #109

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,7 +1,33 @@
 """
     Base.getindex(v::AbstractVector{<:AbstractVector}, r::AbstractRange, datarow::Int)
 
-get a 1-D subset of the every `datarow`-th row of `v::AbstractVector{<:AbstractVector}` within `r::AbstractRange`, used for custom data container for `FlexibleSpacecraft.jl`
+get a 1-D subset of the every `datarow`-th value of the inner vector `v::AbstractVector{<:AbstractVector}` within the specified range `r::AbstractRange`.
+
+# Example
+
+```julia
+> angularvelocity # ::AbstractVector{<:AbstractVector}
+10-element Vector{Vector{Int64}}:
+ [1, 2, 3]
+ [1, 2, 3]
+ ⋮
+ [1, 2, 3]
+ [1, 2, 3]
+
+> angularvelocity[1]
+3-element Vector{Int64}: # get the first vector
+ 1
+ 2
+ 3
+
+> angularvelocity[1:5, 1] # get the 1st element of the 1st to 5th vector of the
+ 5-element Vector{Int64}:
+  1
+  1
+  1
+  1
+  1
+```
 """
 function Base.getindex(v::AbstractVector{<:AbstractVector}, r::Union{AbstractRange, Colon}, datarow::Int)
     return getindex.(v[r], datarow)
@@ -10,7 +36,29 @@ end
 """
     Base.getindex(v::AbstractVector{<:AbstractVector}, r::Int, datarow::Int)
 
-get an element of the `v<:SVector`, used for custom data container for `FlexibleSpacecraft.jl`
+get `datarow`-th element of the `r`-th vector in the `v::AbstractVector{<:AbstractVector}`
+
+# Example
+
+```julia
+> angularvelocity # ::AbstractVector{<:AbstractVector}
+10-element Vector{Vector{Int64}}:
+ [1, 2, 3]
+ [1, 2, 3]
+ ⋮
+ [1, 2, 3]
+ [1, 2, 3]
+
+> angularvelocity[1]
+3-element Vector{Int64}: # get the first vector
+ 1
+ 2
+ 3
+
+> angularvelocity[3, 2] # get the 2st element of the 3rd vector of the `angularvelocity`
+2
+```
+
 """
 function Base.getindex(v::AbstractVector{<:AbstractVector}, r::Int, datarow::Int)
     return getindex(v[r], datarow)
