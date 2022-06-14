b61ebd988c14e3ae18c2c83399a3fedbc0375a60
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-06-14 11:51:17 +0000

    test script for disturbance input to structure #116

--- test/main.jl
+++ test/main.jl
@@ -5,11 +5,11 @@ using .FlexibleSpacecraft
 
 # define parameter for the spacecraft
 paramfilepath = "./test/spacecraft2.yml"
-(simconfig, attitudemodel, distconfig, initvalue, orbitinfo, strparam, strmodel) = readparamfile(paramfilepath)
+(simconfig, attitudemodel, distconfig, initvalue, orbitinfo, strparam, strmodel, strdistconfig) = readparamfile(paramfilepath)
 
 # run simulation
 println("Begin simulation!")
-@time (time, attitudedata, orbitdata, strdata) = runsimulation(attitudemodel, strmodel, initvalue, orbitinfo, distconfig, simconfig)
+@time (time, attitudedata, orbitdata, strdata) = runsimulation(attitudemodel, strmodel, initvalue, orbitinfo, distconfig, strdistconfig, simconfig)
 println("Completed!")
 
 @test quaternion_constraint(attitudedata.quaternion)
