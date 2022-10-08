61161a3adfdda015d8dc3106cae4777b77d605ed
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-10-08 13:43:03 +0900

    fig internal I/F of `AttitudeDisturbance.jl`

--- src/AttitudeDynamics/AttitudeDisturbance.jl
+++ src/AttitudeDynamics/AttitudeDisturbance.jl
@@ -46,23 +46,38 @@ function set_attitudedisturbance(distconfigdict::AbstractDict)::DisturbanceConfi
     return distconfig
 end
 
+"""
+    calc_attitudedisturbance
+
+calculate disturbance torque input to the attitude dynamics
+"""
+function calc_attitudedisturbance(distconfig::DisturbanceConfig, inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)::Vector
+
+    disturbance = zeros(3) + _constant_torque(distconfig.consttorque)
+
+    if distconfig.gravitationaltorque == true
+        disturbance = disturbance + _gravity_gradient(inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)
+    end
+
+    return disturbance
+end
 
 """
-    constant_torque(constant_torque::Vector)
+    _constant_torque(torque_config::AbstractVector{<:Real})
 
-Function that returns given constant torque vector
+Function that returns the predefined constant disturbance torque vector
 """
-constant_torque(C_ECI2Body::Union{Matrix{<:Real}, SMatrix{3, 3, <:Real}}, constant_torque::SVector{3, <:Real})::SVector{3} = transpose(C_ECI2Body) * constant_torque
-
-"""
-    constant_torque(C_ECI2Body::SMatrix{3, 3, <:Real}, constant_torque::Nothing)
-
-Function that returns zero vector
-"""
-constant_torque(C_ECI2Body::Union{Matrix{<:Real}, SMatrix{3, 3, <:Real}}, constant_torque::Nothing)::SVector{3} = zeros(SVector{3})
+_constant_torque(torque_config::AbstractVector{<:Real})::SVector{3} = torque_config
 
 """
-    function gravity_gradient_torque(inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)
+    _constant_torque(torque_config::Nothing)
+
+    Function that returns zero disturbance torque for attitude dynamics
+        """
+        _constant_torque(torque_config::Nothing)::SVector{3} = zeros(SVector{3})
+
+"""
+    _gravity_gradient(inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)
 
 Function that returns gravity gradient torque
 
@@ -75,7 +90,7 @@ Function that returns gravity gradient torque
 * Z-vector of LVLH frame
 
 """
-function gravity_gradient_torque(inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)
+function _gravity_gradient(inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)
 
     # Transformation matrix from LVLH to spacecraft body frame
     C = C_ECI2Body * inv(C_ECI2LVLH)
@@ -95,22 +110,6 @@ function gravity_gradient_torque(inertia, orbit_angular_velocity, C_ECI2Body, C_
     torque_vector = 3*orbit_angular_velocity^2 * nadir_skew * inertia * nadir_vector;
 
     return torque_vector
-end
-
-"""
-    calc_attitudedisturbance
-
-calculate disturbance torque input to the attitude dynamics
-"""
-function calc_attitudedisturbance(distconfig::DisturbanceConfig, inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)::Vector
-
-    disturbance = zeros(3) + constant_torque(C_ECI2Body, distconfig.consttorque)
-
-    if distconfig.gravitationaltorque == true
-        disturbance = disturbance + gravity_gradient_torque(inertia, orbit_angular_velocity, C_ECI2Body, C_ECI2LVLH, LVLHframe_z)
-    end
-
-    return disturbance
 end
 
 end
