e17c197506627207f2d4e2d4a8e5bc8c9fc840b1
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-10-16 21:02:03 +0900

    add struct `SimData` and its I/F

--- src/Core/SimulationCore.jl
+++ src/Core/SimulationCore.jl
@@ -8,11 +8,34 @@ module SimulationCore
 using LinearAlgebra, StaticArrays, ProgressMeter
 using ..Frames, ..OrbitBase, ..AttitudeDisturbance, ..DynamicsBase, ..KinematicsBase, ..StructuresBase, ..StructureDisturbance, ..ParameterSettingBase, ..AttitudeControlBase
 
-export runsimulation
+export SimData, runsimulation
+
+"""
+    SimData
+
+data container for one simulation result.
+
+# Fields
+
+* `time::StepRangeLen`: time information
+* `datanum::Unsigned`: numbers of simulation data
+* `attitude::AttitudeData`: data container for attitude dynamics
+* `appendages::AppendageData`: data container for the appendages
+* `orbit::OrbitData`: data contaier for orbit data
+
+"""
+struct SimData
+    time::StepRangeLen
+    datanum::Unsigned
+    attitude::AttitudeData
+    appendages::AppendageData
+    orbit::OrbitData
+end
+
 
 ############# runsimulation function ##################################
 """
-    runsimulation(attitudemodel, initvalue::KinematicsBase.InitData, orbitinfo::OrbitBase.OrbitInfo, distconfig::DisturbanceConfig, simconfig::SimulationConfig)::Tuple
+    runsimulation
 
 Function that runs simulation of the spacecraft attitude-structure coupling problem
 
@@ -27,17 +50,12 @@ Function that runs simulation of the spacecraft attitude-structure coupling prob
 
 # Return value
 
-Return is tuple of `(time, attidata, orbitdata, strdata``)`
-
-* `time`: 1-D array of the time
-* `attidata`: Struct of time trajectory of the physical amount states of the spacecraft system
-* `orbitdata`: Struct of the orbit state trajectory
-* `strdata`: Struct of the trajectory of the state of the flexible appendage
+return value is the instance of `SimData`
 
 # Usage
 
 ```julia
-(time, attitudedata, orbitdata, strdata) = runsimulation(attitudemodel, strmodel, initvalue, orbitinfo, distconfig, simconfig)
+simdata = runsimulation(attitudemodel, strmodel, initvalue, orbitinfo, distconfig, simconfig)
 ```
 
 """
@@ -51,55 +69,55 @@ Return is tuple of `(time, attidata, orbitdata, strdata``)`
     strdistconfig::AbstractStrDistConfig,
     simconfig::SimulationConfig,
     attitude_controller
-    )::Tuple
+    )::SimData
 
     ##### Constants
-    # Sampling time
+    # Sampling tl.time
     Ts = simconfig.samplingtime
 
     # transformation matrix from ECI frame to orbital plane frame
     C_ECI2OrbitPlane = OrbitBase.ECI2OrbitalPlaneFrame(orbitinfo.orbitalelement)
 
     # Data containers
-    (datanum, time, attidata, strdata, orbitdata) = _init_datacontainers(simconfig, initvalue, strmodel, orbitinfo)
+    tl = _init_datacontainers(simconfig, initvalue, strmodel, orbitinfo)
 
     ### main loop of the simulation
-    prog = Progress(datanum, 1, "Simulation running...", 50)   # progress meter
-    for iter = 1:datanum
+    prog = Progress(tl.datanum, 1, "Simulation running...", 50)   # progress meter
+    for iter = 1:tl.datanum
 
         ### orbit state
-        orbitdata.angularvelocity[iter] = get_angular_velocity(orbitinfo.orbitmodel)
-        orbitdata.angularposition[iter] = orbitdata.angularvelocity[iter] * time[iter]
+        tl.orbit.angularvelocity[iter] = get_angular_velocity(orbitinfo.orbitmodel)
+        tl.orbit.angularposition[iter] = tl.orbit.angularvelocity[iter] * tl.time[iter]
 
         # calculation of the LVLH frame and its transformation matrix
-        C_OrbitPlane2RAT = OrbitalPlaneFrame2RadialAlongTrack(orbitinfo.orbitalelement, orbitdata.angularvelocity[iter], time[iter])
+        C_OrbitPlane2RAT = OrbitalPlaneFrame2RadialAlongTrack(orbitinfo.orbitalelement, tl.orbit.angularvelocity[iter], tl.time[iter])
         C_ECI2RAT = C_OrbitPlane2RAT * C_ECI2OrbitPlane
         C_ECI2LVLH = C_ECI2RAT
 
         ### attitude state
         # Update current attitude
-        C_ECI2Body = ECI2BodyFrame(attidata.quaternion[iter])
-        attidata.bodyframe[iter] = C_ECI2Body * UnitFrame
+        C_ECI2Body = ECI2BodyFrame(tl.attitude.quaternion[iter])
+        tl.attitude.bodyframe[iter] = C_ECI2Body * UnitFrame
 
         # update the roll-pitch-yaw representations
         C_RAT2Body = C_ECI2Body * transpose(C_ECI2RAT)
         C_LVLH2Body = C_ECI2Body * transpose(C_ECI2LVLH)
         # euler angle from RAT to Body frame is the roll-pitch-yaw angle of the spacecraft
         current_RPY = dcm2euler(C_LVLH2Body)
-        attidata.eulerangle[iter] = current_RPY
+        tl.attitude.eulerangle[iter] = current_RPY
         # RPYframe representation can be obtained from the LVLH unit frame
-        attidata.RPYframe[iter] = C_LVLH2Body * LVLHUnitFrame
+        tl.attitude.RPYframe[iter] = C_LVLH2Body * LVLHUnitFrame
 
         ### flexible appendages state
         if isnothing(strmodel)
             # do nothing
         else
-            strdata.physicalstate[iter] = modalstate2physicalstate(strmodel, strdata.state[iter])
+            tl.appendages.physicalstate[iter] = modalstate2physicalstate(strmodel, tl.appendages.state[iter])
         end
 
         ### input to the attitude dynamics
         # disturbance input
-        attidistinput = transpose(C_ECI2Body) * calc_attitudedisturbance(distconfig, distinternals, attitudemodel.inertia, time[iter], orbitdata.angularvelocity[iter], C_ECI2Body, C_ECI2RAT, orbitdata.LVLH[iter].z, Ts)
+        attidistinput = transpose(C_ECI2Body) * calc_attitudedisturbance(distconfig, distinternals, attitudemodel.inertia, tl.time[iter], tl.orbit.angularvelocity[iter], C_ECI2Body, C_ECI2RAT, tl.orbit.LVLH[iter].z, Ts)
         # control input
         attictrlinput = transpose(C_ECI2Body) * control_input!(attitude_controller, current_RPY, [0, 0, 0])
 
@@ -109,12 +127,12 @@ Return is tuple of `(time, attidata, orbitdata, strdata``)`
             strctrlinput = nothing
         else
             # disturbance input
-            strdistinput = calcstrdisturbance(strdistconfig, time[iter])
+            strdistinput = calcstrdisturbance(strdistconfig, tl.time[iter])
             # control input
             strctrlinput = 0
             # data log
-            strdata.controlinput[iter] = strctrlinput
-            strdata.disturbance[iter] = strdistinput
+            tl.appendages.controlinput[iter] = strctrlinput
+            tl.appendages.disturbance[iter] = strdistinput
         end
 
         ### attitude-structure coupling dynamics
@@ -124,7 +142,7 @@ Return is tuple of `(time, attidata, orbitdata, strdata``)`
             strvelocity = 0
             attiinput = 0
         else
-            currentstrstate = strdata.state[iter]
+            currentstrstate = tl.appendages.state[iter]
             if iter == 1
                 if typeof(currentstrstate) <: AbstractVector
                     straccel = currentstrstate[(strmodel.DOF+1):end] / Ts
@@ -135,40 +153,40 @@ Return is tuple of `(time, attidata, orbitdata, strdata``)`
                 end
             else
                 if typeof(currentstrstate) <: AbstractVector
-                    previousstrstate = strdata.state[iter-1]
+                    previousstrstate = tl.appendages.state[iter-1]
                     straccel = (currentstrstate[(strmodel.DOF+1):end] - previousstrstate[(strmodel.DOF+1):end]) / Ts
                     strvelocity = currentstrstate[(strmodel.DOF+1):end]
                 else
-                    previousstrstate = strdata.state[iter-1]
+                    previousstrstate = tl.appendages.state[iter-1]
                     straccel = (currentstrstate - previousstrstate) / Ts
                     strvelocity = currentstrstate
                 end
             end
             # angular velocity of the attitude dynamics for the structural coupling input
-            attiinput = attidata.angularvelocity[iter]
+            attiinput = tl.attitude.angularvelocity[iter]
         end
 
         ### Time evolution of the system
-        if iter != datanum
+        if iter != tl.datanum
 
             # Update angular velocity
-            attidata.angularvelocity[iter+1] = update_angularvelocity(attitudemodel, time[iter], attidata.angularvelocity[iter], Ts, attidata.bodyframe[iter], attidistinput, attictrlinput, straccel, strvelocity)
+            tl.attitude.angularvelocity[iter+1] = update_angularvelocity(attitudemodel, tl.time[iter], tl.attitude.angularvelocity[iter], Ts, tl.attitude.bodyframe[iter], attidistinput, attictrlinput, straccel, strvelocity)
 
             # Update quaternion
-            attidata.quaternion[iter+1] = update_quaternion(attidata.angularvelocity[iter], attidata.quaternion[iter], Ts)
+            tl.attitude.quaternion[iter+1] = update_quaternion(tl.attitude.angularvelocity[iter], tl.attitude.quaternion[iter], Ts)
 
             # Update the state of the flexible appendages
             if isnothing(strmodel)
                 # do nothing
             else
-                strdata.state[iter+1] = update_strstate(strmodel, Ts, time[iter], strdata.state[iter], attiinput, strctrlinput, strdistinput)
+                tl.appendages.state[iter+1] = update_strstate(strmodel, Ts, tl.time[iter], tl.appendages.state[iter], attiinput, strctrlinput, strdistinput)
             end
         end
         # update the progress meter
         next!(prog)
     end
 
-    return (time, attidata, orbitdata, strdata)
+    return tl
 end
 
 function _init_datacontainers(simconfig, initvalue, strmodel, orbitinfo)
@@ -179,15 +197,18 @@ function _init_datacontainers(simconfig, initvalue, strmodel, orbitinfo)
     datanum = floor(Int, simconfig.simulationtime/simconfig.samplingtime) + 1;
 
     # Initialize data containers for the attitude dynamics
-    attidata = initattitudedata(datanum, initvalue)
+    attitude = initattitudedata(datanum, initvalue)
 
     # initialize data container for the structural motion of the flexible appendages
-    strdata = initappendagedata(strmodel, [0, 0, 0, 0], datanum)
+    appendages = initappendagedata(strmodel, [0, 0, 0, 0], datanum)
 
     # initialize orbit state data array
-    orbitdata = initorbitdata(datanum, orbitinfo.planeframe)
+    orbit = initorbitdata(datanum, orbitinfo.planeframe)
 
-    return (datanum, time, attidata, strdata, orbitdata)
+    # initialize simulation data container
+    tl = SimData(time, datanum, attitude, appendages, orbit)
+
+    return tl
 end
 
 end
