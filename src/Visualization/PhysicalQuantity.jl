153e5f57252dd65aa13629d6e5b3c13d5b6d2b21
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-11-03 22:06:52 +0900

    fix type recipes

--- src/Visualization/PhysicalQuantity.jl
+++ src/Visualization/PhysicalQuantity.jl
@@ -13,10 +13,15 @@ export angularvelocities, eulerangles, quaternions
 
 Plots angular velocity of each axis in one figure
 """
-function angularvelocities(time::StepRangeLen, angularvelocity::Vector{StaticArrays.SVector{3, <:Real}}; timerange::Tuple{<:Real, <:Real} = (0, 0))::AbstractPlot
+function angularvelocities(
+    time::StepRangeLen,
+    angularvelocity::Vector{StaticArrays.SVector{3, <:Real}};
+    timerange::Tuple{<:Real, <:Real} = (0, 0)
+    )::AbstractPlot
 
     plotlyjs()
 
+    # using plot recipe for these plots
     plt = plot();
     plt = plot!(plt, time, angularvelocity, 1, timerange = timerange, ylabelname = "Angular velocity (rad/s)", datalabel = "x-axis");
     plt = plot!(plt, time, angularvelocity, 2, timerange = timerange, ylabelname = "Angular velocity (rad/s)", datalabel = "y-axis");
@@ -31,10 +36,15 @@ end
 Plots time history of euler angles
 
 """
-function eulerangles(time::StepRangeLen, eulerangle::Vector{StaticArrays.SVector{3, <:Real}}; timerange::Tuple{<:Real, <:Real} = (0, 0))::AbstractPlot
+function eulerangles(
+    time::StepRangeLen,
+    eulerangle::Vector{SVector{3, <:Real}};
+    timerange::Tuple{<:Real, <:Real} = (0, 0)
+    )::AbstractPlot
 
     plotlyjs()
 
+    # using type recipe
     plt = plot();
     plt = plot!(plt, time, eulerangle, 1, timerange = timerange, ylabelname = "Euler angle (rad)", datalabel = "roll");
     plt = plot!(plt, time, eulerangle, 2, timerange = timerange, ylabelname = "Euler angle (rad)", datalabel = "pitch");
@@ -48,10 +58,15 @@ function quaternions(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector
 
     Plot quaternions in single plot
 """
-function quaternions(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector{4, <:Real}}; timerange::Tuple{<:Real, <:Real} = (0, 0))
+function quaternions(
+    time::StepRangeLen,
+    quaternion::AbstractVector{SVector{4, <:Real}};
+    timerange::Tuple{<:Real, <:Real} = (0, 0)
+    )
 
     plotlyjs()
 
+    # using type recipe
     plt = plot();
     plt = plot!(plt, time, quaternion, 1, timerange = timerange, ylabelname = "Quaternion", datalabel = "q1");
     plt = plot!(plt, time, quaternion, 2, timerange = timerange, ylabelname = "Quaternion", datalabel = "q2");
@@ -61,31 +76,64 @@ function quaternions(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector
     return plt
 end
 
-@recipe function f(time::StepRangeLen, quaternion::Vector{StaticArrays.SVector{4, <:Real}}, index::Integer; timerange = (0, 0), ylabelname = "No name", datalabel = "")
-    if !(1 <= index <= 4)
+# type recipe for data container of quaternion
+@recipe function f(
+    plt::AbstractPlot,
+    time::StepRangeLen,
+    quaternion::AbstractVector{SVector{4, <:Real}},
+    axisindex::Integer;
+    timerange = (0, 0),
+    datalabel = "")
+
+    # check the bounds for indexing
+    if !(1 <= axisindex <= 4)
         throw(BoundsError(quaternion[1], index))
     end
 
     xguide --> "Time (s)"
     yguide --> ylabelname
     label --> datalabel
+    linewidth --> 2
+    fontfamily --> "Times"
+    guidefontsize --> 15
+    tickfontsize --> 15
+    legendfontsize --> 15
+    titlefontsize --> 15
 
     # get the index for data
     dataindex = DataContainers.getdataindex(timerange, convert(Float64, time.step))
 
-    return time[dataindex], quaternion[dataindex, index]
+    return time[dataindex], quaternion[dataindex, axisindex]
 end
 
-@recipe function f(time::StepRangeLen, angularvelocity::Vector{StaticArrays.SVector{3, <:Real}}, axisindex::Integer; timerange = (0, 0), ylabelname = "No name", datalabel = "")
+# type recipe for data container of vector of 3D vectors, mainly for the angles
+@recipe function f(
+    time::StepRangeLen,
+    anglevectors::AbstractVector{SVector{3, <:Real}},
+    axisindex::Integer;
+    timerange = (0, 0),
+    ylabelname = "no y-label",
+    datalabel = "no data label")
+
+    # check the bounds for indexing
+    if !(1 <= axisindex <= 3)
+        throw(BoundsError(anglevectors[1], index))
+    end
 
     xguide --> "Time (s)"
     yguide --> ylabelname
     label --> datalabel
+    linewidth --> 2
+    fontfamily --> "Times"
+    guidefontsize --> 15
+    tickfontsize --> 15
+    legendfontsize --> 15
+    titlefontsize --> 15
 
     # get the index for data
     dataindex = DataContainers.getdataindex(timerange, convert(Float64, time.step))
 
-    return time[dataindex], angularvelocity[dataindex, axisindex]
+    return time[dataindex], anglevectors[dataindex, axisindex]
 end
 
 end
