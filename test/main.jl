21d0aa5a92d7f3c3388e0453d78548d1f828763a
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-11-05 16:11:50 +0900

    update `main.jl` script

--- test/main.jl
+++ test/main.jl
@@ -10,23 +10,21 @@ paramfilepath = "./test/spacecraft.yml"
 # run simulation
 simtime = @timed simdata = runsimulation(attitudemodel, strmodel, initvalue, orbitinfo, orbitinternals, distconfig, distinternals, strdistconfig, strinternals, simconfig, attitudecontroller)
 
+# test the quaternion value to check the stability of the simulation
 @test quaternion_constraint(simdata.attitude.quaternion)
 
-plottime = @timed begin # measure time for post process
+fig1 = plot_angularvelocity(simdata.time, simdata.attitude.angularvelocity)
+fig2 = plot_eulerangles(simdata.time, simdata.attitude.eulerangle)
 
-    fig1 = plot_angularvelocity(simdata.time, simdata.attitude.angularvelocity)
-    fig2 = plot_eulerangles(simdata.time, simdata.attitude.eulerangle)
+anim = framegif(simdata.time, T_UnitFrame2LVLHFrame * UnitFrame, simdata.attitude.RPYframe, Tgif = 5e-1, FPS = 20)
 
-    anim = framegif(simdata.time, T_UnitFrame2LVLHFrame * UnitFrame, simdata.attitude.RPYframe, Tgif = 5e-1, FPS = 20)
+display(fig1)
+display(fig2)
+display(anim)
 
-    display(fig1)
-    display(fig2)
-    display(anim)
-
-    if !isnothing(strmodel)
-        fig3 = plot_physicalstate(simdata.time, simdata.appendages.physicalstate)
-        display(fig3)
-    end
+if !isnothing(strmodel)
+    fig3 = plot_physicalstate(simdata.time, simdata.appendages.physicalstate)
+    display(fig3)
 end
 
 println("Simulation time : $(simtime.time) (s)")
